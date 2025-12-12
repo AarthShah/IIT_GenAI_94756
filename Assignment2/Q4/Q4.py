78e668dfc9a447ce9bf25c7230eeb558103b68bd
AarthShah <aarths123@gmail.com>
2025-12-12 12:16:58 +0530

    Q4

---
+++ Assignment2/Q4/Q4.py
@@ -0,0 +1,13 @@
+import requests
+
+API_key='your_api_key_here'
+
+City_name=input("Enter city name: ")
+
+url=f"https://api.openweathermap.org/data/2.5/weather?q={City_name}&appid={API_key}"
+
+response=requests.get(url)
+data=response.json()
+
+#print(data)
+print(f"Weather in {City_name}: {data['main']['temp']} K , Humidity: {data['main']['humidity']}% , Weather Description: {data['weather'][0]['description']}")

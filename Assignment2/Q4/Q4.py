4784fa6d0fc230b71224f6bbf5daa67c024082b8
AarthShah <aarths123@gmail.com>
2025-12-13 09:08:29 +0530

    creating env file

--- Assignment2/Q4/Q4.py
+++ Assignment2/Q4/Q4.py
@@ -1,6 +1,10 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-API_key='your_api_key_here'
+load_dotenv()
+
+API_key=os.getenv('OpenWeather_API_key')
 
 City_name=input("Enter city name: ")
 
@@ -11,3 +15,4 @@ data=response.json()
 
 #print(data)
 print(f"Weather in {City_name}: {data['main']['temp']} K , Humidity: {data['main']['humidity']}% , Weather Description: {data['weather'][0]['description']}")
+
